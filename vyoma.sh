f6d3b858f2714cdb39c14005d7debd2ca4d17668
malivinayak <malivinayak012@gmail.com>
2022-07-20 16:00:15 +0000

    Update Vyoma.sh

--- vyoma.sh
+++ vyoma.sh
@@ -2,18 +2,6 @@
 
 export PS1='\[\033[01;32m\]vyoma\[\033[00m\] \[\033[01;34m\]\w\[\033[00m\] (uptickpro) $ '
 brew install icarus-verilog
-PYTHON_CONFIGURE_OPTS="--enable-shared" pyenv install 3.8.10
-pyenv global 3.8.10
-pip install --upgrade pip
-pip install cocotb
-clear
-echo "****** UpTickPro (Evaluation Version) 1.0.0 *******"
-echo "Copyright (c) 2022, Vyoma Systems Private Limited"
-echo "All Rights Reserved."
-#!/bin/bash
-
-export PS1='\[\033[01;32m\]vyoma\[\033[00m\] \[\033[01;34m\]\w\[\033[00m\] (uptickpro) $ '
-brew install icarus-verilog
 PYTHON_CONFIGURE_OPTS="--enable-shared" pyenv install 3.8.13
 pyenv global 3.8.13
 pip install --upgrade pip
