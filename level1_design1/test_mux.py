e0d626e397eb84ed85dac806b46aded2bd45990e
malivinayak <malivinayak012@gmail.com>
2022-07-24 07:52:25 +0000

    L1D1 - CTB Python Verification

--- level1_design1/test_mux.py
+++ level1_design1/test_mux.py
@@ -13,7 +13,7 @@ async def test_mux(dut):
         dict[i] = "dut.inp"+ str(i) + ".value"
     
     for i in range(31):                                 
-        exec("dut.inp%d.value = %d" % (i,randrange(4)))        #  Assigning inp0 to inp30
+        exec("dut.inp%d.value = %d" % (i,randrange(4)))        # Assigning inp0 to inp30
         dut.sel.value = i                                      # Assigning out
         await Timer(1, "ns")
         if(dut.out.value != eval(dict[i])):
